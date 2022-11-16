0c1ae760c21fa6d506aa6554c31133a0300329ba
Vlad Paulechka <vlad9.ru@gmail.com>
2022-11-16 16:28:15 +0300

    создание обьектов формы

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Form.css'
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [subject, setSubject] = useState('physical');
+
+    const onChangeCounty = (e) =>{
+        setCountry(e.target.value)
+    }
+
+    const onChangeCity = (e) =>{
+        setCity(e.target.value)
+    }
+
+    const onChangeSubject = (e) =>{
+        setSubject(e.target.value)
+    }
+
     return (
-        <div>
-            Form
+        <div className={'form'}>
+            <h3>Введите ваши данные</h3>
+            <input className={'input'} type="text" placeholder={'Страна'} value={country} onChange={onChangeCounty}/>
+            <input className={'input'} type="text" placeholder={'Улица'} value={city} onChange={onChangeCity}/>
+
+            <select className={'select'} value={subject} onChange={onChangeSubject}>
+                <option value={"physical"}>Физ. лицо</option>
+                <option value={"legal"}>Юр. лицо</option>
+            </select>
         </div>
     );
 };
