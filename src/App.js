7b61aa2203f3921b5d030e4c6e54492a835914dc
Vlad Paulechka <vlad9.ru@gmail.com>
2022-11-16 13:15:13 +0300

    install commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import * as React from 'react';
-
+import {useTelegram} from "./hooks/useTelegram";
+import {useEffect} from "react";
 
 function App() {
+    const {onToggleButton, tg} = useTelegram()
 
+    useEffect(()=>{
+        tg.ready();
+    }, [])
 
-
-  return (
-    <div className="App">
-      <button onClick={onClose} type="button">Click Me</button>
-    </div>
-  );
+    return (
+        <div className="App">
+          <button onClick={onToggleButton} type="button">toggle</button>
+        </div>
+    );
 }
 
 export default App;
