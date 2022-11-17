e793ad49a3f89dd97229b6e7bf30d0e1943f359a
Vlad Paulechka <vlad9.ru@gmail.com>
2022-11-17 17:01:01 +0300

    add product

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,19 +3,30 @@ import './Form.css'
 import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
-    const [country, setCountry] = useState('');
-    const [street, setStreet] = useState('');
-    const [subject, setSubject] = useState('physical');
+    const [FIO, setFIO] = useState('');
+    const [city, setCity] = useState('');
+    const [work_experience, setWorkExperience] = useState('');
+    const [instagram, setInstagram] = useState('');
+    const [english, setEnglish] = useState('');
+    const [position, setPosition] = useState('');
+    const [phone_number, setPhoneNumber] = useState('');
+    const [salary, setSalary] = useState('');
     const {tg} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
-            country,
-            street,
-            subject
+            FIO,
+            city,
+            work_experience,
+            instagram,
+            english,
+            position,
+            phone_number,
+            salary
         }
         tg.sendData(JSON.stringify(data));
-    }, [country, street, subject, tg])
+    }, [FIO, city, work_experience, instagram, english, position, phone_number, salary, tg])
+
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
@@ -24,31 +35,56 @@ const Form = () => {
         }
     }, [onSendData, tg])
 
+
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
         })
     }, [tg])
 
+
     useEffect(() => {
-        if(!street || !country) {
+        if(!FIO || !city) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    }, [country, street, tg])
+    }, [FIO, city, tg])
 
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value)
+
+    const onChangeFIO = (e) => {
+        setFIO(e.target.value)
     }
 
-    const onChangeStreet = (e) => {
-        setStreet(e.target.value)
+    const onChangePhoneNumber = (e) => {
+        setPhoneNumber(e.target.value)
     }
 
-    const onChangeSubject = (e) => {
-        setSubject(e.target.value)
+    const onChangeInstagram = (e) => {
+        setInstagram(e.target.value)
     }
+
+    const onChangeSalary = (e) => {
+        setSalary(e.target.value)
+    }
+
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value)
+    }
+
+    const onChangeWorkExperience = (e) => {
+        setWorkExperience(e.target.value)
+    }
+
+    const onChangeEnglish = (e) => {
+        setEnglish(e.target.value)
+    }
+
+    const onChangePosition = (e) => {
+        setPosition(e.target.value)
+    }
+
 
     return (
         <div className={"form"}>
@@ -56,21 +92,80 @@ const Form = () => {
             <input
                 className={'input'}
                 type="text"
-                placeholder={'Страна'}
-                value={country}
-                onChange={onChangeCountry}
+                placeholder={'ФИО'}
+                value={FIO}
+                onChange={onChangeFIO}
             />
+
+            <select value={city} onChange={onChangeCity} className={'select'}>
+                <option disabled>Гражданство</option>
+                <option value={'1'}>РБ</option>
+                <option value={'2'}>РФ</option>
+                <option value={'3'}>КАЗ</option>
+                <option value={'4'}>Другой</option>
+            </select>
+
+            <select value={work_experience} onChange={onChangeWorkExperience} className={'select'}>
+                <option disabled>Опыт работы</option>
+                <option value={'1'}>1</option>
+                <option value={'2'}>1-3</option>
+                <option value={'3'}>3-5</option>
+                <option value={'4'}>'>'5</option>
+            </select>
+
+            <select value={english} onChange={onChangeEnglish} className={'select'}>
+                <option disabled>Английский</option>
+                <option value={'1'}>А1 (Beginner)</option>
+                <option value={'2'}>А2 (Elementary)</option>
+                <option value={'3'}>В1 (Intermediate)</option>
+                <option value={'4'}>В2 (Upper Intermediate)</option>
+                <option value={'5'}>С1 (Advanced)</option>
+                <option value={'6'}>С2 (Proficiency)</option>
+            </select>
+
+            <select value={position} onChange={onChangePosition} className={'select'}>
+                <option value="" hidden>Выбрать</option>
+                <optgroup label="Семейство «Тыквенные»">
+                    <option value={'1'}>Арбуз</option>
+                    <option value={'2'}>Дыня</option>
+                    <option value={'3'}>Тыква</option>
+                </optgroup>
+                <optgroup label="Семейство «Паслёновые»">
+                    <option value={'5'}>Баклажан</option>
+                    <option value={'6'}>Картофель</option>
+                    <option value={'7'}>Томат</option>
+                    <option value={'8'}>Перец стручковый</option>
+                </optgroup>
+            </select>
+
+            <input
+                className={'input'}
+                type="tel"
+                placeholder={'Номер телефона'}
+                value={phone_number}
+                onChange={onChangePhoneNumber}
+            />
+
             <input
                 className={'input'}
                 type="text"
-                placeholder={'Улица'}
-                value={street}
-                onChange={onChangeStreet}
+                placeholder={'Instagram'}
+                value={instagram}
+                onChange={onChangeInstagram}
             />
-            <select value={subject} onChange={onChangeSubject} className={'select'}>
-                <option value={'physical'}>Физ. лицо</option>
-                <option value={'legal'}>Юр. лицо</option>
-            </select>
+
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'ЗП'}
+                value={salary}
+                onChange={onChangeSalary}
+            />
+
+            {/*<select value={subject} onChange={onChangeSubject} className={'select'}>*/}
+            {/*    <option value={'physical'}>Физ. лицо</option>*/}
+            {/*    <option value={'legal'}>Юр. лицо</option>*/}
+            {/*</select>*/}
         </div>
     );
 };
